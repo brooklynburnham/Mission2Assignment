cee90ba5b79740b84ad3df61a2ffcb358e557d1a
brooklynburnham <167155861+brooklynburnham@users.noreply.github.com>
2025-01-20 10:51:14 -0700

    New changes

--- DiceRoller.cs
+++ DiceRoller.cs
@@ -1,6 +1,37 @@
-namespace Mission2Assignment;
+using System;
 
-public class Roll
+namespace Mission2Assignment
 {
-    
+    public class DiceRoller
+    {
+        public int[] RollDice(int numRolls)
+        {
+            int[] results = new int[13]; // Array indices 2-12 will be used
+
+            Random r = new Random();
+            for (int i = 0; i < numRolls; i++)
+            {
+                int num1 = r.Next(1, 7); // Random number between 1 and 6
+                int num2 = r.Next(1, 7); // Random number between 1 and 6
+                int sum = num1 + num2;
+                results[sum]++;
+            }
+            return results;
+        }
+    }
 }
+
+
+
+// layout
+// keep track of roll, list how results came out
+// 1. ask user how many times to roll
+// 2. get their input
+// for 'user input' then 
+// 3. roll die 1 and collect result 
+// 4. roll die 2 and collect result
+// 5. add together and store result
+// 6. for each number possible to be rolled
+// print the number 
+// calculate the % of times it was rolled
+// print an asterisk for each % it was rolled
--- Program.cs
+++ Program.cs
@@ -1,3 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Mission2Assignment;
 
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numRolls = 0;
+
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        System.Console.WriteLine("How many dice rolls would you like to simulate?");
+        numRolls = int.Parse(System.Console.ReadLine());
+
+        DiceRoller roller = new DiceRoller();
+        int[] results = roller.RollDice(numRolls);
+
+        System.Console.WriteLine("ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        System.Console.WriteLine("Total number of rolls = " + numRolls + ".");
+
+// get histogram and %
+        for (int i = 2; i <= 12; i++)
+        {
+            int percentage = (int)((results[i] / (double)numRolls) * 100);
+            System.Console.WriteLine($"{i}: ");
+            System.Console.WriteLine(new string('*', percentage));
+        }
+
+        System.Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+
+    }
+}
+
+// layout
+// keep track of roll, list how results came out
+// 1. ask user how many times to roll
+// 2. get their input
+// for 'user input' then 
+    // 3. roll die 1 and collect result 
+    // 4. roll die 2 and collect result
+    // 5. add together and store result
+// 6. for each number possible to be rolled
+    // print the number 
+    // calculate the % of times it was rolled
+    // print an asterisk for each % it was rolled
